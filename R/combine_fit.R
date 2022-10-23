58137097b0d250bb3d1a08435979c7faec020f8e
Haiyang Jin <hjin317@aucklanduni.ac.nz>
2022-10-23 13:12:23 +0400

    add opt to conduct bridge sampling

--- R/combine_fit.R
+++ R/combine_fit.R
@@ -1,12 +1,13 @@
 #' @title Combine multiple files of fit objects
 #' @description combine the fit objects saved in separate files.
 #'
-#' @param path path to the fit objects; will be use as \code{path} in \code{list.files()}.
+#' @param path path to the fit objects; will be use as `path` in `list.files()`.
 #' @param pattern wildcard used to identify the single fit objects; will be use
-#' as \code{pattern} in \code{list.files()}.
+#' as `pattern` in `list.files()`.
 #' @param outfn the output file name of the combined fit objects. If outfn is
-#' \code{NULL} (default), no file will be saved locally.
-#' @param ... other arguments to be used in \code{list.files()}.
+#' `NULL` (default), no file will be saved locally.
+#' @param bs whether perform bridge sampling and save the result. Default to FALSE.
+#' @param ... other arguments to be used in `brms::bridge_sampler`.
 #'
 #' @return a combined fit object.
 #' @export
@@ -14,11 +15,12 @@
 #' @examples
 #'\dontrun{
 #' combine_fit("brmfit*", outfn="combined_brmfit")
+#' combine_fit("brmfit*", outfn="combined_brmfit", bs=TRUE)
 #'}
 
-combine_fit <- function(path=".", pattern, outfn=NULL, ...){
+combine_fit <- function(path=".", pattern, outfn=NULL, bs=FALSE, ...){
   # list all files
-  filelist <- list.files(path, pattern, full.names=T, ...)
+  filelist <- list.files(path, pattern, full.names=T)
   N_fit <- length(filelist)
 
   # read the first rds
@@ -34,8 +36,18 @@ combine_fit <- function(path=".", pattern, outfn=NULL, ...){
   if (!is.null(outfn)){
     saveRDS(combined_fit, file = outfn)
   }
-
   message(sprintf("%d files of fitted objects were combined!", N_fit))
+
+  # perform bridge sampling if needed
+  if (bs) {
+    message(sprintf("\nBridge sampling for %s...", outfn))
+
+    bs_fit <- brms::bridge_sampler(combined_fit, ...)
+
+    if (!is.null(outfn)){
+      saveRDS(bs_fit, file = paste0(substr(outfn,1,nchar(outfn)-4),"_bs.rds"))
+    }
+  }
 
   return(combined_fit)
 }
