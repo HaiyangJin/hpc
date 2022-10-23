c11bf4e7f3a4ba849561f2f839f205cc5b925bda
Haiyang Jin <hjin317@aucklanduni.ac.nz>
2022-10-23 20:29:28 +0400

    update to set link to func

--- R/combine_fit.R
+++ R/combine_fit.R
@@ -1,13 +1,13 @@
 #' @title Combine multiple files of fit objects
 #' @description combine the fit objects saved in separate files.
 #'
-#' @param path path to the fit objects; will be use as `path` in `list.files()`.
+#' @param path path to the fit objects; will be use as `path` in [list.files()].
 #' @param pattern wildcard used to identify the single fit objects; will be use
-#' as `pattern` in `list.files()`.
+#' as `pattern` in [list.files()].
 #' @param outfn the output file name of the combined fit objects. If outfn is
 #' `NULL` (default), no file will be saved locally.
 #' @param bs whether perform bridge sampling and save the result. Default to FALSE.
-#' @param ... other arguments to be used in `brms::bridge_sampler`.
+#' @param ... other arguments to be used in [brms::bridge_sampler()].
 #'
 #' @return a combined fit object.
 #' @export
